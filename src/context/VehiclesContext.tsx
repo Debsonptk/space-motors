50a7f4cd66ae7ff6fa399742e537128d8fa505a1
DebsonCoelho <debsoncoelh@gmail.com>
2023-07-31 13:55:08 -0300

    add assets

--- src/context/VehiclesContext.tsx
+++ src/context/VehiclesContext.tsx
@@ -1,18 +1,52 @@
-import { createContext, useContext, useMemo } from 'react'
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react'
+
+import Api from 'services/api'
+
+import { VehiclesType } from 'types/VehiclesType'
 
 interface IContextProps {
   something: string
 }
 
-interface IMyCustomProviderProps {
+interface IVehiclesProviderProps {
   children: React.ReactNode
 }
 
 export const ReactContext = createContext<IContextProps>({} as IContextProps)
 
-export const VehiclesProvider: React.FC<IMyCustomProviderProps> = ({
+export const VehiclesProvider: React.FC<IVehiclesProviderProps> = ({
   children,
 }) => {
+  const [vehicles, setVehicles] = useState<VehiclesType[]>([])
+  const [isLoading, setIsloading] = useState(false)
+
+  const fetchVehicles = useCallback(async () => {
+    setIsloading(true)
+
+    try {
+      const {
+        data: { results },
+      } = await Api.get(`/vehicles`)
+      setVehicles(results)
+    } catch (e) {
+      // eslint-disable-next-line no-console
+      console.error(e)
+    } finally {
+      setIsloading(false)
+    }
+  }, [])
+
+  useEffect(() => {
+    fetchVehicles()
+  }, [fetchVehicles])
+
   return (
     <ReactContext.Provider
       value={useMemo(
